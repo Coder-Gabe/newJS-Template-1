6c2f475f8b3304b0499073227780dfc54e0266fd
Rexkwondo <87598098+Rexkwondo@users.noreply.github.com>
2023-04-12 07:02:41 -0700

    Hundo commit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,44 @@
-function displayMessage() {
-	let msg = document.getElementById('message').value;
-	//alert(msg);
+// get start and end numbers from the inputs
 
-	Swal.fire(
-		{
-			backdrop: false,
-			title: 'App Name',
-			text: msg
-		}
-	);
+function getValues() {
+	let startValue = document.getElementById('startValue').value;
+	let endValue = document.getElementById('endValue').value;
+
+	let startNumber = parseInt(startValue);
+	let endNumber = parseInt(endValue);
+
+	let numberArray = generateNumbers(startNumber, endNumber);
+	displayNumbers(numberArray);
 }
+
+// generate the range of numbers to display
+function generateNumbers(start, end) {
+
+	let basketOfNumbers = [];
+
+	for (let number = 0; number <= 100; number = number + 1) {
+
+		basketOfNumbers.push(number);
+
+	}
+
+	return basketOfNumbers;
+
+}
+
+//placing the generate numbers on the page and bolding the even numbers
+function displayNumbers(numbers) {
+
+	let results = '';
+
+	for (let index = 0; index < numbers.length; index = index + 1) {
+
+		let currentNumber = numbers[index];
+
+		results = results + "<tr><td>" + currentNumber + "</td></tr>";
+	}
+
+	let tableBody = document.getElementById('results');
+	tableBody.innerHTML = results;
+}
+
